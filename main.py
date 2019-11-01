7b83c806cbce80e210637e2212aa0cc333a2ed16
Luis <porta5648@gmail.com>
2019-11-01 00:36:03 -0600

    validacion de tamaño de palabra

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 listaDePrueba = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 llave = "cab"
-palabra = input("introduzca la palabra a encriptar: ").lower()
+palabra = input("introduzca la palabra a encriptar (3 letras): ").lower()
 
-for x,y in zip(palabra,llave):
-    p =  listaDePrueba.index(x) + 1
-    l =  listaDePrueba.index(y)
-    encrip = p+l
-    print(listaDePrueba[encrip],end="")
+if len(palabra) == len(llave):
+    for x,y in zip(palabra,llave):
+        p =  listaDePrueba.index(x) + 1
+        l =  listaDePrueba.index(y)
+        encrip = p+l
+        print(listaDePrueba[encrip],end="")
+
+else:
+    print("la palabra es demasiado grande!")
+
+
     
