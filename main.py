254ba50401d95d6ee2a933035509ca17bf29234c
Luis Roman <porta5648@gmail.com>
2019-11-01 16:03:05 -0600

    cambio de nombre en variables

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3.6
 
-listaDePrueba = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+abecedario = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 llave = "cab"
 palabra = input("introduzca la palabra a encriptar (3 letras): ").lower()
 
 if len(palabra) == len(llave):
     for x,y in zip(palabra,llave):
-        p =  listaDePrueba.index(x) + 1
-        l =  listaDePrueba.index(y)
+        p =  abecedario.index(x) + 1
+        l =  abecedario.index(y)
         encrip = p+l
-        print(listaDePrueba[encrip].upper(),end="")
+        print(abecedario[encrip].upper(),end="")
 
 else:
     print("la palabra es demasiado grande!")
